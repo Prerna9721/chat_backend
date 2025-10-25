f55648f9d3c93922f487c7f83da322c50a179114
Prerna Sinha <prernasinha9511@gmail.com>
2025-10-25 14:46:30 +0530

    Committing

--- src/main/java/com/substring/chat/ChatApplication/config/AppConstants.java
+++ src/main/java/com/substring/chat/ChatApplication/config/AppConstants.java
@@ -3,6 +3,6 @@ package com.substring.chat.ChatApplication.config;
 public class AppConstants {
 	//
 	
-	public static final String FRONT_END_BASE_URL="http://localhost:5174";
+	public static final String FRONT_END_BASE_URL="https://chat-app-orpin-eight-99.vercel.app";
 
 }
